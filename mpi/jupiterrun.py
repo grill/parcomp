9764bba5d4987aad9736a556bb935deb3f0ef70a
alex <alex_steiner@gmx.net>
2013-01-22 13:36:49 +0100

    jupiterrun.py works now

--- mpi/jupiterrun.py
+++ mpi/jupiterrun.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import sys
+import os
 
 usage = \
 "usage: jupiterrun.py [number of threads] [first node] [programm name]"
@@ -31,5 +32,5 @@ command += prog
 
 #print(command)
 
-execvp("mpirun", command)
+os.execvp("mpirun", command)
 
