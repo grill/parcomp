4c2fe8269231a7c694a2b1d188ff3a13aaddcb4b
alex <alex_steiner@gmx.net>
2013-01-22 13:31:25 +0100

    python script for easy running of stuff on jupiter

---
+++ mpi/jupiterrun.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import sys
+
+usage = \
+"usage: jupiterrun.py [number of threads] [first node] [programm name]"
+
+if(len(sys.argv) < 4):
+    print(usage)
+    sys.exit(1)
+
+threads = int(sys.argv[1])
+node = int(sys.argv[2])
+prog = sys.argv[3:]
+
+#print(threads)
+#print(node)
+#print(prog)
+
+command = ["mpirun"]
+
+while(threads > 0):
+    if(threads >= 8):
+        command += ["-host", "jupiter%d" % node, "-np", "8"]
+        node += 1
+        threads -= 8
+    else:
+        command += ["-host", "jupiter%d" % node, "-np" , str(threads)]
+        threads = 0
+
+command += prog
+
+#print(command)
+
+execvp("mpirun", command)
+
