da319cd4ee1084f545b51cd09bb49551314abffd
alex <alex_steiner@gmx.net>
2013-01-26 18:37:33 +0100

    now has an option to control iteration count and works (as far as i can tell)

--- mpi/stencil-computation/stencil-computation.c
+++ mpi/stencil-computation/stencil-computation.c
@@ -15,13 +15,14 @@
 #define HELLO 1234 // tag for control messages
 #define SCAN 1337
 #define STENCIL 4242
-#define X(A,i,j) A[(i)*(m+2)+(j)]
+#define X(A,i,j) A[(i)+(j)*(n+2)]
 
 struct opt {
     INT_T m;
     INT_T n;
     INT_T r;
     INT_T c;
+    int iterations;
     int parallel;
 };
 
@@ -31,6 +32,28 @@ INT_T* compute(INT_T *A, INT_T *B, int iterations, INT_T n, INT_T m, INT_T r, IN
 void synchronize(INT_T* A, INT_T n, INT_T m, INT_T r, INT_T c, MPI_Comm comm, MPI_Datatype column_t) ;
 void gen_data(INT_T *A, INT_T n, INT_T m) ;
 int check(INT_T *A, INT_T n, INT_T m, int iterations) ;
+int rank2rid(int rank, int r, int c);
+int rank2cid(int rank, int r, int c);
+int getrank(int rid, int cid, int r, int c);
+
+int rank2rid(int rank, int r, int c) {
+    return rank % r;
+}
+int rank2cid(int rank, int r, int c) {
+    return rank / r;
+}
+int getrank(int rid, int cid, int r, int c) {
+    if(cid < 0) {
+        cid += c;
+    }
+    cid %= c;
+    if(rid < 0) {
+        rid += r;
+    }
+    rid %= r;
+
+    return (cid * r) + rid;
+}
 
 int main(int argc, char ** argv) {
   int comm_size, rank;
@@ -52,10 +75,18 @@ int main(int argc, char ** argv) {
     printf("only have %d processes, should have (%ld*%ld=) %ld\n", comm_size, args.r, args.c, args.r*args.c);
     MPI_Finalize();
     return 1;
-  } else if (args.r % 2 != 0 || args.c % 2 != 0) {
+  } else if ((args.r != 1 && args.r % 2 != 0) || (args.c != 1 && args.c % 2 != 0)) {
     printf("r and c must be 1 or even\n");
     MPI_Finalize();
     return 1;
+  } else if (args.n % args.r != 0 || args.m % args.c != 0) {
+    printf("n must be divisable by r and m must be divisable by c\n");
+    MPI_Finalize();
+    return 1;
+  }
+  
+  if(rank == 0) {
+    printf("stencil computation, n=%ld, m=%ld, r=%ld, c=%ld, i=%d\n", args.n, args.m, args.r, args.c, args.iterations);
   }
 
   n = args.n/args.r;
@@ -66,19 +97,22 @@ int main(int argc, char ** argv) {
 
   gen_data(A, n, m);
 
-  MPI_Type_vector(n,1,m+2,INT_MPI_T,&column_t);
+  MPI_Type_vector(m,1,n+2,INT_MPI_T,&column_t);
+  MPI_Type_commit(&column_t);
 
   MPI_Barrier(MPI_COMM_WORLD);
   time = - MPI_Wtime();
 
-  C = compute(A, B, 0, n, m, args.r, args.c, MPI_COMM_WORLD, column_t);
+  C = compute(A, B, args.iterations, n, m, args.r, args.c, MPI_COMM_WORLD, column_t);
 
   time += MPI_Wtime();
 
-  if(check(C, n, m, 0)) {
-    printf("check successfull!\n");
+  if(check(C, n, m, args.iterations)) {
+    //printf("check successfull!\n");
+    printf("%d: %lf\n", rank, time);
   } else {
-    printf("check failed!\n");
+    //printf("check failed!\n");
+    printf("F: %d: %lf\n", rank, time);
   }
 
 #ifdef DEBUGDEBUG
@@ -119,6 +153,70 @@ INT_T* compute(INT_T *A, INT_T *B, int iterations, INT_T n, INT_T m, INT_T r, IN
     return A;
 }
 
+
+void synchronize(INT_T* A, INT_T n, INT_T m, INT_T r, INT_T c, MPI_Comm comm, MPI_Datatype column_t) {
+    int rank, other;
+    int rid, cid;
+    
+    MPI_Comm_rank(comm, &rank);
+    rid = rank2rid(rank, r, c);
+    cid = rank2cid(rank, r, c);
+
+    //rows of processors must synchronize up and down
+    if(r == 1) {
+        memcpy(A + (n+2)*(m+1)+1, A + n + 3, n*sizeof(INT_T));
+        memcpy(A + 1, A + (n+2)*(m)+1, n*sizeof(INT_T));
+    } else {
+        if(rid % 2 == 0) {
+            //up
+            other = getrank(rid - 1, cid, r, c);
+            MPI_Send(A + n + 3, n, INT_MPI_T, other, STENCIL, comm);
+            MPI_Recv(A + 1, n, INT_MPI_T, other, STENCIL, comm, MPI_STATUS_IGNORE);
+            //down
+            other = getrank(rid + 1, cid, r, c);
+            MPI_Recv(A + (n+2)*(m+1)+1, n, INT_MPI_T, other, STENCIL, comm, MPI_STATUS_IGNORE);
+            MPI_Send(A + (n+2)*m+1, n, INT_MPI_T, other, STENCIL, comm);
+        } else {
+            //down
+            other = getrank(rid + 1, cid, r, c);
+            MPI_Recv(A + (n+2)*(m+1)+1, n, INT_MPI_T, other, STENCIL, comm, MPI_STATUS_IGNORE);
+            MPI_Send(A + (n+2)*m+1, n, INT_MPI_T, other, STENCIL, comm);
+            //up
+            other = getrank(rid - 1, cid, r, c);
+            MPI_Send(A + n + 3, n, INT_MPI_T, other, STENCIL, comm);
+            MPI_Recv(A + 1, n, INT_MPI_T, other, STENCIL, comm, MPI_STATUS_IGNORE);
+        }
+    }
+    //columns of processors must synchronize left and right
+    if(c == 1) {
+        for(INT_T i = 1; i <= m; i++) {
+            X(A,0,i) = X(A,n,i);
+            X(A,n+1,i) = X(A,1,i);
+        }
+    } else {
+        if(cid % 2 == 0) {
+            //left
+            other = getrank(rid, cid - 1, r, c);
+            MPI_Send(A + n + 3, 1, column_t, other, STENCIL, comm);
+            MPI_Recv(A + n + 2, 1, column_t, other, STENCIL, comm, MPI_STATUS_IGNORE);
+            //right
+            other = getrank(rid, cid + 1, r, c);
+            MPI_Recv(A + 2*n + 3, 1, column_t, other, STENCIL, comm, MPI_STATUS_IGNORE);
+            MPI_Send(A + 2*n + 2, 1, column_t, other, STENCIL, comm);
+        } else {
+            //right
+            other = getrank(rid, cid + 1, r, c);
+            MPI_Recv(A + 2*n + 3, 1, column_t, other, STENCIL, comm, MPI_STATUS_IGNORE);
+            MPI_Send(A + 2*n + 2, 1, column_t, other, STENCIL, comm);
+            //left
+            other = getrank(rid, cid - 1, r, c);
+            MPI_Send(A + n + 3, 1, column_t, other, STENCIL, comm);
+            MPI_Recv(A + n + 2, 1, column_t, other, STENCIL, comm, MPI_STATUS_IGNORE);
+        }
+    }
+}
+    
+/*
 void synchronize(INT_T* A, INT_T n, INT_T m, INT_T r, INT_T c, MPI_Comm comm, MPI_Datatype column_t) {
     if(r == 1 && c == 1) {
         memcpy(A + (n+1)*(m+2), A + m + 3, m*sizeof(INT_T));
@@ -196,6 +294,7 @@ void synchronize(INT_T* A, INT_T n, INT_T m, INT_T r, INT_T c, MPI_Comm comm, MP
         }
     }
 }
+*/
 
 void gen_data(INT_T *A, INT_T n, INT_T m) {
     for(INT_T i = 1; i <= n; i++) {
@@ -222,10 +321,11 @@ void parse_args(int argc, char ** argv, struct opt* args) {
     args->m = 1024;
     args->r = 1;
     args->c = 1;
+    args->iterations = 1;
     
     char c;
 
-    while ((c = getopt (argc, argv, "n:m:c:r:")) != -1)
+    while ((c = getopt (argc, argv, "n:m:c:r:i:")) != -1)
      switch (c)
        {
        case 'n':
@@ -239,6 +339,9 @@ void parse_args(int argc, char ** argv, struct opt* args) {
          break;
        case 'r':
          args->r = strtol(optarg, NULL, 16);
+         break;
+       case 'i':
+         args->iterations = strtol(optarg, NULL, 10);
          break;
        case '?':
          if (optopt == 's')
